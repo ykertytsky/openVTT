f654a27e31274d69e411ac2812ba5658ec71f8f0
ykertytsky <y.kertytsky@gmail.com>
2025-01-08 14:55:26 +0200

    look of instrument panel implemented

--- client/src/components/InstrumentPanel/InstrumentPanel.tsx
+++ client/src/components/InstrumentPanel/InstrumentPanel.tsx
@@ -1,15 +1,46 @@
 import React from "react";
 
-import { Tile, Button } from "@carbon/react";
-import { Cursor, Pen, Notebook, Ruler } from "@carbon/icons-react";
+import { Button } from "@carbon/react";
+import { Cursor_2, Pen, Notebook, Ruler } from "@carbon/icons-react";
 
 import './InstrumentPanel.scss'
 
 const InstrumentPanel = () => {
   return (
-    <Tile className="Instruments">
-      <Ruler size={20}/>
-    </Tile>
+    <div className="Instruments">
+      <Button
+        hasIconOnly={true}
+        kind="secondary"
+        tooltipPosition={"right"}
+        tooltipAlignment={"center"}
+        renderIcon={Cursor_2}
+        iconDescription={"Cursor"}
+      ></Button>
+      <Button
+        hasIconOnly={true}
+        kind="secondary"
+        tooltipPosition={"right"}
+        tooltipAlignment={"center"}
+        renderIcon={Pen}
+        iconDescription={"Pen"}
+      ></Button>
+      <Button
+        hasIconOnly={true}
+        kind="secondary"
+        renderIcon={Notebook}
+        tooltipPosition={"right"}
+        tooltipAlignment={"center"}
+        iconDescription={"Notes"}
+      ></Button>
+      <Button
+        hasIconOnly={true}
+        kind="secondary"
+        tooltipPosition={"right"}
+        tooltipAlignment={"center"}
+        renderIcon={Ruler}
+        iconDescription={"Ruler"}
+      ></Button>
+    </div>
   );
 }
 
